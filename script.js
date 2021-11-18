04f4f05ae315343aa1cef54ee33191af9e454c11
Navera <naverazafar23@gmail.com>
2021-11-17 17:43:50 -0800

    created cleanUpIndex() function

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 /* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+function cleanUpIndex() {
+    const contacts = document.querySelectorAll('div.contact')
+
+    contacts.forEach(function(item) {
+        item.remove();
+    })
+}
+
+
+
+// const contactList = [  
+// 	{ 
+// 		name: "Oliver Queen", 
+// 		phone: "778-555-1234", 
+// 		address: "101 Main St, Star City, USA",    
+// 		email: "greenarrow@watchtower.com",  
+// 	},   
+// 	{    
+// 		name: "Jessica Cruz",    
+// 		phone: "123-555-5555",    
+// 		address: "Portland Oregon",    
+// 		email: "greenlantern@watchtower.com",  
+// 	}
+// ]
+
+// function createSingleIndex(object) {
+//     const newContact = document.querySelector('.main')
+
+//     console.log(newContact.insertAdjacentHTML('afterend', object.name))
+// }
+
+// createSingleIndex(contactList)

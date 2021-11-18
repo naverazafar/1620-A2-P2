5dda86bd0c41eb4bb0c2087069673ecd8d57b11f
Navera <naverazafar23@gmail.com>
2021-11-18 11:03:33 -0800

    changed createSingleIndex from using template literals to insertAdjacentHTML

--- script.js
+++ script.js
@@ -10,10 +10,35 @@ function cleanUpIndex() {
 
 
 function createSingleIndex(object) {
-    return `
-        <div>
-            <p>${object[0].name}</p>
-        </div>
-    `
+    const newContact = document.querySelector('div.main')
+
+    newContact.insertAdjacentHTML('beforeend', object[0].name)
 }
 
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},   
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
+
+const contact = {
+
+    name: "Oliver Queen", 
+	phone: "778-555-1234", 
+	address: "101 Main St, Star City, USA",    
+	email: "greenarrow@watchtower.com",
+    
+}
+
+
+
+console.log(createSingleIndex(contactList))

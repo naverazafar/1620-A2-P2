71a003e8d07ed4efd14d0a7bbff0d52122ad67bb
Navera <naverazafar23@gmail.com>
2021-11-20 13:00:51 -0800

    added code to renderView() and made sure it worked

--- script.js
+++ script.js
@@ -63,26 +63,41 @@ function cleanUpView() {
 
 function renderView(object) {
 	const div = document.querySelector("div.main")
-	div.classList.add("contactinfo")
+	div.classList.add("contactinfo", "contactedit")
+	const image = '<img src="img/profile.jpg"></img>'
+	const buttonedit = '<button>Edit</button>'
+	const buttonclose = '<button>Close</button>'
 
+	div.insertAdjacentHTML('afterbegin', createDiv(buttonedit + buttonclose))
 	div.insertAdjacentHTML('afterbegin', createDiv(object.address))
 	div.insertAdjacentHTML('afterbegin', createDiv(object.phone))
 	div.insertAdjacentHTML('afterbegin', createDiv(object.email))
-	div.insertAdjacentHTML('afterbegin', createDiv(object.name))
+	div.insertAdjacentHTML('afterbegin', createDiv(object.name + image))
 	
 
 	const name = document.querySelector('div.main > div')
-	name.classList.add("contactname")
+	name.classList.add('contactname')
+
+	const pic = document.querySelector('div.main > div > img')
+	pic.classList.add('profilepic')
 
 	const email = document.querySelector('div.main > div + div')
 	email.classList.add('contactemail')
-	email.classList.add('input')
 	
 	const phone = document.querySelector('div.main > div + div + div')
 	phone.classList.add('contactphone')
 
 	const address = document.querySelector('div.main > div + div + div + div')
 	address.classList.add('contactaddress')
+
+	const buttons = document.querySelector('div.main > div + div + div + div + div')
+	buttons.classList.add('buttons')
+
+	const buttonOne = document.querySelector('div.main > div + div + div + div + div > button')
+	buttonOne.classList.add('button', 'edit')
+
+	const buttonTwo = document.querySelector('div.main > div + div + div + div + div > button + button')
+	buttonTwo.classList.add('button', 'close')
 
 }
 
